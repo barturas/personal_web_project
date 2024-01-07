8581a20fca8c6dc3d78ca78f10ddda738b6f73f2
barturas <barturas.testing@gmail.com>
2024-01-07 22:25:57 +0200

    new styles

---
+++ script.js
@@ -0,0 +1,22 @@
+const words = "Crystal ball, show me the future"; // add more words as needed
+
+const ANIMATION_DURATION = 4000; //ns
+
+// separate each character into it's own div
+const characters = words.split("").forEach((char, i) => {
+    function createElement(offset) {
+        const div = document.createElement("div");
+        div.innerText = char;
+        div.classList.add("character");
+        div.style.animationDelay = `-${i * (ANIMATION_DURATION / 10) - offset}ms`;
+        return div;
+
+    }
+
+    document.getElementById("spiral").append(createElement(0));
+    document
+        .getElementById("spiral2")
+        .append(createElement(-1 * (ANIMATION_DURATION / 5)));
+
+
+});
